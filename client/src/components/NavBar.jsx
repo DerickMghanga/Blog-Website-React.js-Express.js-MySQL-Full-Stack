1b1138e687dd6153165c258d12c4baf3b1b98f57
DerickMghanga <derickmghanga3329@gmail.com>
2024-01-08 05:05:23 +0300

    Navbar design completed

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import Logo from "../img/blog-icon.png";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='navbar'>
+      <div className="container">
+        <div className="logo">
+          <img src={Logo} alt="app-logo" />
+        </div>
+        <div className="links">
+          <Link className='link' to="/?cat=art">
+            <h5>ART</h5>
+          </Link>
+          <Link className='link' to="/?cat=science">
+            <h5>SCIENCE</h5>
+          </Link>
+          <Link className='link' to="/?cat=technology">
+            <h5>TECHNOLOGY</h5>
+          </Link>
+          <Link className='link' to="/?cat=cinema">
+            <h5>CINEMA</h5>
+          </Link>
+          <Link className='link' to="/?cat=design">
+            <h5>DESIGN</h5>
+          </Link>
+          <Link className='link' to="/?cat=food">
+            <h5>FOOD</h5>
+          </Link>
+          <span>Derick</span>
+          <span>Logout</span>
+          <span className='write'>
+            <Link className='link' to="/write">Write</Link>
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }
 
