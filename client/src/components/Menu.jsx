e3363285d7e78965966cfdd863dd97b007198c99
DerickMghanga <derickmghanga3329@gmail.com>
2024-01-16 23:45:17 +0300

    other posts fetch fixed

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,33 +1,50 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
-const Menu = () => {
+const Menu = ({cat}) => {
 
-    const posts = [
-        {
-          id: 1,
-          title: "Etiam ultricies nisi vel augue",
-          desc: "Curabitur blandit mollis lacus. Vivamus consectetuer hendrerit lacus. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nunc nonummy metus. Nam pretium turpis et arcu. Nulla porta dolor. Mauris sollicitudin fermentum libero. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor.",
-          img: "https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        },
-        {
-          id: 2,
-          title: "Etiam ultricies nisi vel augue",
-          desc: "Curabitur blandit mollis lacus. Vivamus consectetuer hendrerit lacus. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nunc nonummy metus. Nam pretium turpis et arcu. Nulla porta dolor. Mauris sollicitudin fermentum libero. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor.",
-          img: "https://images.pexels.com/photos/267569/pexels-photo-267569.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        },
-        {
-          id: 3,
-          title: "Etiam ultricies nisi vel augue",
-          desc: "Curabitur blandit mollis lacus. Vivamus consectetuer hendrerit lacus. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nunc nonummy metus. Nam pretium turpis et arcu. Nulla porta dolor. Mauris sollicitudin fermentum libero. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor.",
-          img: "https://images.pexels.com/photos/965117/pexels-photo-965117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        },
-        {
-          id: 4,
-          title: "Etiam ultricies nisi vel augue",
-          desc: "Curabitur blandit mollis lacus. Vivamus consectetuer hendrerit lacus. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nunc nonummy metus. Nam pretium turpis et arcu. Nulla porta dolor. Mauris sollicitudin fermentum libero. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor.",
-          img: "https://images.pexels.com/photos/261579/pexels-photo-261579.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        },
-      ];
+    // const posts = [
+    //     {
+    //       id: 1,
+    //       title: "Etiam ultricies nisi vel augue",
+    //       desc: "Curabitur blandit mollis lacus. Vivamus consectetuer hendrerit lacus. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nunc nonummy metus. Nam pretium turpis et arcu. Nulla porta dolor. Mauris sollicitudin fermentum libero. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor.",
+    //       img: "https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    //     },
+    //     {
+    //       id: 2,
+    //       title: "Etiam ultricies nisi vel augue",
+    //       desc: "Curabitur blandit mollis lacus. Vivamus consectetuer hendrerit lacus. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nunc nonummy metus. Nam pretium turpis et arcu. Nulla porta dolor. Mauris sollicitudin fermentum libero. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor.",
+    //       img: "https://images.pexels.com/photos/267569/pexels-photo-267569.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    //     },
+    //     {
+    //       id: 3,
+    //       title: "Etiam ultricies nisi vel augue",
+    //       desc: "Curabitur blandit mollis lacus. Vivamus consectetuer hendrerit lacus. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nunc nonummy metus. Nam pretium turpis et arcu. Nulla porta dolor. Mauris sollicitudin fermentum libero. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor.",
+    //       img: "https://images.pexels.com/photos/965117/pexels-photo-965117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    //     },
+    //     {
+    //       id: 4,
+    //       title: "Etiam ultricies nisi vel augue",
+    //       desc: "Curabitur blandit mollis lacus. Vivamus consectetuer hendrerit lacus. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nunc nonummy metus. Nam pretium turpis et arcu. Nulla porta dolor. Mauris sollicitudin fermentum libero. Suspendisse faucibus, nunc et pellentesque egestas, lacus ante convallis tellus, vitae iaculis lacus elit id tortor.",
+    //       img: "https://images.pexels.com/photos/261579/pexels-photo-261579.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    //     },
+    //   ];
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+      const fetchPost = async () => {
+        try {
+          const res = await axios.get(`/posts?cat=${cat}`);
+          setPosts(res.data);
+          //console.log(res.data);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+  
+      fetchPost();
+    }, [cat]);
 
   return (
     <div className='menu'>
